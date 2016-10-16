4a2dca6588f7c169bc0dea8104cd0ceb32d95c06
Hoang Ho Hai Dang <1353049@student.hcmus.edu.vn>
2016-10-16 23:04:14 +0700

    secure_asset

--- resources/views/frontend/partials/htmlheader.blade.php
+++ resources/views/frontend/partials/htmlheader.blade.php
@@ -5,20 +5,20 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="keywords" content="This is a template" />
     <!-- //for-mobile-apps -->
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all" />
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
-    <link href="{{ asset('css/contactstyle.css') }}" rel="stylesheet" type="text/css" media="all" />
-    <link href="{{ asset('css/faqstyle.css') }}" rel="stylesheet" type="text/css" media="all" />
-    <link href="{{ asset('css/single.css') }}" rel='stylesheet' type='text/css' />
-    <link href="{{ asset('css/medile.css') }}" rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css/bootstrap.css', Request::secure()) }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset('css/style.css', Request::secure()) }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset('css/contactstyle.css', Request::secure()) }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset('css/faqstyle.css', Request::secure()) }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset('css/single.css', Request::secure()) }}" rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css/medile.css', Request::secure()) }}" rel='stylesheet' type='text/css' />
     <!-- banner-slider -->
-    <link href="{{ asset('css/jquery.slidey.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/jquery.slidey.min.css', Request::secure()) }}" rel="stylesheet">
     <!-- pop-up -->
-    <link href="{{ asset('css/popuo-box.css') }}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{ asset('css/popuo-box.css', Request::secure()) }}" rel="stylesheet" type="text/css" media="all" />
     <!-- font-awesome icons -->
-    <link href="{{ asset('css/font-awesome.min.css') }}"  rel="stylesheet"/>
+    <link href="{{ asset('css/font-awesome.min.css', Request::secure()) }}"  rel="stylesheet"/>
     <!-- banner-bottom-plugin -->
-    <link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet" type="text/css" media="all">
+    <link href="{{ asset('css/owl.carousel.css', Request::secure()) }}" rel="stylesheet" type="text/css" media="all">
 
     @yield('page_plugin_css')
 
